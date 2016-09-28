a62057001deeec405bd950d808191cca5755ca34
Olivier TERRIEN <oterrien@neuf.fr>
2016-09-28 17:53:56 +0200

    Update LinkedHashMapTest.java

--- LinkedHashMapTest.java
+++ LinkedHashMapTest.java
@@ -6,8 +6,6 @@ public class LinkedHashMapTest{
     @Test
     public void testLinkedHashMap() {
 
-        System.out.println(Boolean.parseBoolean("true"));
-
         final int MAX_ENTRIES = 5;
 
         LinkedHashMap lhm = new LinkedHashMap(MAX_ENTRIES + 1, .75F, false) {
@@ -16,6 +14,7 @@ public class LinkedHashMapTest{
                 	return size() > MAX_ENTRIES;
             	}
         };
+	    
         lhm.put(0, "H");
         lhm.put(1, "E");
         lhm.put(2, "L");
